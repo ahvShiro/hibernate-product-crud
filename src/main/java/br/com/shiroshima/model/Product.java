bb4aaa86bf20bbe20b86bbe1945269b74cc264bd
ahvShiro <arthurshiro123@gmail.com>
2025-08-14 16:11:18 -0300

    feat: created the class Product

---
+++ src/main/java/br/com/shiroshima/model/Product.java
@@ -0,0 +1,77 @@
+package br.com.shiroshima.model;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Table(name = "products")
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Product(){}
+
+    public Product(String nome, String description, Double price) {
+        this.name = nome;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Product produto)) return false;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "\nProduct{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
